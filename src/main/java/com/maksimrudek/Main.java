3435d6b78692f07044444ceea7ddca1d242f86c0
Maksim Rudek <maksim.rudek@gmail.com>
2023-03-19 10:04:11 +0200

    Init

--- src/main/java/com/maksimrudek/Main.java
+++ src/main/java/com/maksimrudek/Main.java
@@ -1,52 +1,51 @@
 package com.maksimrudek;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpHeaders;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
 
-    public static void main(String[] args) {
-        String prompt = "Hi, how are you doing today?";
-        String response = getAnswer(prompt);
-        System.out.println(response);
-    }
+    public static void main(String[] args) throws URISyntaxException {
+        String prompt = "Hello, how are you?";
+        int maxTokens = 10;
+        String apiKey = "<your-api-key>";
+        String engineId = "<your-engine-id>";
 
-    public static String getAnswer(String prompt) {
+        String requestBody = String.format("{\"prompt\": \"%s\", \"max_tokens\": %d, \"temperature\": 0.5}", prompt, maxTokens);
+
+        HttpClient httpClient = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(String.format("https://api.openai.com/v1/engines/%s/completions", engineId)))
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + apiKey)
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody, StandardCharsets.UTF_8))
+                .build();
+
+        HttpResponse<String> response = null;
         try {
-            URL url = new URL("https://api.openai.com/v1/engines/text-davinci-003/completions");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("Authorization", "Bearer " + "");
-            connection.setDoOutput(true);
-
-            String body = "{\"prompt\": \"" + prompt + "\", \"max_tokens\": 150, \"temperature\": 0.7}";
-
-            OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), StandardCharsets.UTF_8);
-            writer.write(body);
-            writer.flush();
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            StringBuilder response = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                response.append(line);
-            }
-            reader.close();
-
-            int startIndex = response.indexOf("\"text\": \"") + 9;
-            int endIndex = response.indexOf("\"", startIndex);
-            String answer = response.substring(startIndex, endIndex);
-
-            return answer;
-        } catch (IOException e) {
+            response = httpClient.send(request, BodyHandlers.ofString());
+        } catch (Exception e) {
             e.printStackTrace();
-            return "Error: " + e.getMessage();
+        }
+
+        if (response != null && response.statusCode() < 400) {
+            String responseBody = response.body();
+            String[] completionParts = responseBody.split("\"text\":\"");
+            if (completionParts.length > 1) {
+                String generatedText = completionParts[1].split("\"")[0].replace("\\n", "");
+                System.out.println(generatedText);
+            } else {
+                System.err.println(responseBody);
+            }
+        } else {
+            System.err.println("Error: " + response);
         }
     }
 }
